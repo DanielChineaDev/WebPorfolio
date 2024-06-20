f917bf40c724c9897ec836487a22abf11f151d8e
DanielChineaDev <l3lueart@gmail.com>
2024-06-20 16:22:14 +0100

    Adjusted, font size, capital letters, the text and the photograph have also been correctly centered

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,16 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '1rem 2rem', alignItems: 'center' }}>
+    <nav className="fixed top-0 left-0 w-full text-white z-50 flex justify-between items-center p-10">
       <div style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>DANIEL CHINEA</div>
-      <div>
-        <Link href="/about" style={{ margin: '0 1rem' }}>
+      <div className={`uppercase tracking-widest text-xl	`}>
+        <Link href="/about" style={{ margin: '0 3rem' }}>
           About
         </Link>
-        <Link href="/portfolio" style={{ margin: '0 1rem' }}>
+        <Link href="/portfolio" style={{ margin: '0 3rem' }}>
           Portfolio
         </Link>
-        <Link href="/contact" style={{ margin: '0 1rem' }}>
+        <Link href="/contact" style={{ margin: '0 0 0 3rem' }}>
           Contact
         </Link>
       </div>
