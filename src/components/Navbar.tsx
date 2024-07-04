bb6cdb6f187c42bef767207a063fdacca90323fe
DanielChineaDev <l3lueart@gmail.com>
2024-07-04 13:24:28 +0100

    Logo added to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 
 interface NavbarProps {
   onSectionChange: (sectionIndex: number) => void;
@@ -8,15 +9,21 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ onSectionChange }) => {
   return (
     <nav className="fixed top-0 left-0 w-full text-white z-50 flex justify-between items-center p-10">
-      <div style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Image
+          src="/images/logotipo.png"
+          alt="Logo"
+          width={40}
+          height={40}
+        />
         <span
           onClick={() => onSectionChange(0)}
-          style={{ margin: "0 3rem", cursor: "pointer" }}
+          style={{ marginLeft: "1rem", fontSize: "1.5rem", fontWeight: "bold", cursor: "pointer" }}
         >
           DANIEL CHINEA
         </span>
       </div>
-      <div className={`uppercase tracking-widest text-xl`}>
+      <div className="uppercase tracking-widest text-xl">
         <span
           onClick={() => onSectionChange(1)}
           style={{ margin: "0 1rem", cursor: "pointer" }}
